5e136e035f13b01972f8f2e7f7725116caba1dcc
hamidr-source <hr454837@gmail.com>
2024-07-26 17:26:09 +0330

    Add Info and Summary components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
 import React from "react";
 import Map from "./components/Map";
+import Info from "./components/Info"
+import Summary from "./components/Summary";
 
 export default function App() {
-  return <Map />;
+  return (
+    <div className="container mt-3">
+      <div className="row">
+        <div className="col col-md-9">
+          <Map />
+        </div>
+        <div className="col-12 col-md-3">
+          <Info />
+        </div>
+      </div>
+      <div className="row mt-3">
+        <Summary />
+      </div>
+    </div>
+  );
 }
