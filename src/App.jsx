fed38f3c81ad96e298ae474b480d243a040ab475
hamidr-source <hr454837@gmail.com>
2024-07-26 18:01:49 +0330

    Fix country name bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Map from "./components/Map";
-import Info from "./components/Info"
+import Info from "./components/Info";
 import Summary from "./components/Summary";
 
 export default function App() {
+  const [selectedCountry, setSelectedCountry] = useState("iran");
+
+  function handleClick (countryName) {
+    setSelectedCountry(countryName)
+  }
   return (
     <div className="container mt-3">
       <div className="row">
         <div className="col col-md-9">
-          <Map />
+          <Map handleClick={handleClick} />
         </div>
         <div className="col-12 col-md-3">
           <Info />
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -3,6 +3,14 @@ import map from "./coords.json";
 import ImageMapper from "react-image-mapper";
 import IMAGE_URL from "./map.jpg";
 
-export default function Map() {
-  return <ImageMapper src={IMAGE_URL} map={map} width={800} imgWidth={1000} />;
+export default function Map({ handleClick }) {
+  return (
+    <ImageMapper
+      onClick={(e) => handleClick(e.name)}
+      src={IMAGE_URL}
+      map={map}
+      width={800}
+      imgWidth={1000}
+    />
+  );
 }
